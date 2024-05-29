20ea8206fda3a7456e24263478e6d0001bd624e9
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-05-29 16:15:44 +0200

    implementacion de las  clases del seguimiento academico y del sistema de formacion

---
+++ src/main/java/org/example/Subject.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Subject {
+    private String subjectId;
+    private String name;
+    private int credits;
+
+    // Constructor
+    public Subject(String subjectId, String name, int credits) {
+        this.subjectId = subjectId;
+        this.name = name;
+        this.credits = credits;
+    }
+
+    // Getters
+    public String getSubjectId() {
+        return subjectId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+}
+
